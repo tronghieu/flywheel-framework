bbd4ee35e2a2992cd4152e0098921061918c4949
Trong Hieu Luu <luutronghieu@alimama.vn>
2013-07-08 12:16:12 +0700

    Add Pagination widget

---
+++ Flywheel/Html/Widget/Pagination.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Flywheel\Html\Widget;
+
+use Flywheel\Controller\Widget;
+use Flywheel\Factory;
+
+class Pagination extends Widget {
+    public $htmlOptions = array();
+    public $pageSize;
+    public $total;
+    public $currentPage = 1;
+    public $numPageShow	=7;
+    public $showTotalPage	= false;
+    public $showJumpPage	= true;
+    public $showSelectPage	= false;
+    public $totalPage;
+    public $startPage;
+    public $items = '';
+    public $route = array();
+    /**
+     * @var \Flywheel\Router\WebRouter;
+     */
+    public $router;
+
+    protected function _init() {
+        if (null == $this->viewFile) {
+            $this->viewFile = 'pagination';
+        }
+
+        if (!$this->viewPath) {
+            $this->viewPath = __DIR__ .'/template/';
+        }
+
+        $this->router = Factory::getRouter();
+
+        $this->totalPage = ceil($this->total / $this->pageSize);
+        if (($this->currentPage < 0) || ($this->currentPage > $this->totalPage)) {
+            $this->currentPage = 1;
+        }
+
+        if ($this->currentPage > $this->numPageShow / 2) {
+            $this->startPage = $this->currentPage - floor($this->numPageShow / 2);
+            if(($this->totalPage - $this->startPage) < $this->numPageShow) {
+                $this->startPage = $this->totalPage - $this->numPageShow + 1;
+            }
+        } else {
+            $this->startPage = 1;
+        }
+
+        if ($this->startPage < 1) {
+            $this->startPage = 1;
+        }
+    }
+
+    public function createLink($page) {
+        $params = array_slice($this->route, 1);
+        $params['page'] = $page;
+        return $this->router->createUrl($this->route[0], $params);
+    }
+}
