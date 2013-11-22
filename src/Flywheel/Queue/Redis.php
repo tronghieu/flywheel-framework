6339ef2324bf8c682f85f7479f5b2f25d0de333b
Trong Hieu Luu <luutronghieu@alimama.vn>
2013-11-22 11:27:35 +0700

    Add more method working with queue.

--- src/Flywheel/Queue/Redis.php
+++ src/Flywheel/Queue/Redis.php
@@ -1,20 +1,22 @@
 <?php
 namespace Flywheel\Queue;
+use Flywheel\Redis\Connection;
+
 class Redis extends BaseQueue {
     /**
-     * @var \Flywheel\Redis\Connection;
+     * @var Connection;
      */
     protected $_adapter;
 
     /**
-     * @return \Flywheel\Redis\Connection
+     * @return Connection
      */
     public function getAdapter() {
         if (null == $this->_adapter) {
             $t = explode('/',$this->_config['dsn']);
             $db = isset($t[1])? $t[1] : 0;
             $t = explode(':', $t[0]);
-            $redis = new \Flywheel\Redis\Connection();
+            $redis = new Connection();
             $redis->connect($t[0], $t[1]);
             if (isset($this->_config['auth'])) {
                 $redis->auth($this->_config['auth']);
@@ -25,8 +27,16 @@ class Redis extends BaseQueue {
         return $this->_adapter;
     }
 
+    public function prepend($member) {
+        return $this->getAdapter()->rPush($this->_name, $member);
+    }
+
     public function push($member) {
         return $this->getAdapter()->lPush($this->_name, $member);
+    }
+
+    public function shift($member) {
+        return $this->getAdapter()->lPop($this->_name);
     }
 
     public function pop() {
@@ -39,6 +49,10 @@ class Redis extends BaseQueue {
 
     public function members() {
         return $this->getAdapter()->lRange($this->_name, 0, -1);
+    }
+
+    public function getIndex($index) {
+        return $this->getAdapter()->lIndex($this->_name, $index);
     }
 
     public function isExists($member) {
