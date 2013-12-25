9bac8dfdf871ce785a9c25b66551760b8ccfc22f
Hoang Ngoc Tan <hoangngoctan@alimama.vn>
2013-12-25 08:58:46 +0700

    Added Asset Management for Flywheel

--- src/Flywheel/Asset/Asset.php
+++ src/Flywheel/Asset/Asset.php
@@ -1,32 +1,30 @@
 <?php
 
 /**
- * Asset Management for Flywheel Framework
- * $config = array(
-        'envi' => 'dev',
-        'combine' => true,
-        'minify' => true,
-        'base_url' => '',
-        'assets_path' => 'E:\Copy\uwamp\www\alm2\www_html\mobile\assets',
-        'assets_dir' => 'assets',
-        'base_path' => 'assets',
-        'cache_dir' => 'cache',
-        'cache_path' => 'E:\Copy\uwamp\www\alm2\www_html\mobile\assets\cache', //
-        'cache_url' => 'cache', // base_url/cache_dr
-        'js_dir' => 'js',
-        'js_path' => 'js', //
-        'js_url' => 'js',
-        'css_dir' => 'css',
-        'css_path' => 'css', //
-        'css_url' => 'css',
+  $config = array(
+  'envi' => 'dev',
+  'combine' => true,
+  'minify' => true,
+  'base_url' => '',
+  'assets_path' => 'path/to/asset/folder',
+  'assets_dir' => 'assets',
+  'base_path' => 'assets',
+  'cache_dir' => 'cache',
+  'cache_path' => 'path/to/asset/folder', //
+  'cache_url' => 'cache', // base_url/cache_dr
+  'js_dir' => 'js',
+  'js_path' => 'js', //
+  'js_url' => 'js',
+  'css_dir' => 'css',
+  'css_path' => 'css', //
+  'css_url' => 'css',
   );
- * 
- * How to use
- * 
- * $cache = new \Flywheel\Asset\Asset;
- * $cache->js('file.css','group_name');
- * $cache->js('js.css','group_name');
- * $cache->display(); 
+
+
+  $cache = new \Flywheel\Asset\Asset;
+  $cache->js('file.css','group_name');   //add css file to group 'group_name'
+  $cache->js('file.css','group_name');   //add js file to group 'group_name'
+  $cache->display();  //display assets
  * @author tradade
  */
 
