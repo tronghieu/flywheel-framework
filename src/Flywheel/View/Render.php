d1b4e02e320dd73d4e8652802ffec8d30c1f559d
Lưu Trọng Hiếu <luutronghieu@alimama.vn>
2014-12-25 08:30:31 +0700

    Fix double slash in template path

--- src/Flywheel/View/Render.php
+++ src/Flywheel/View/Render.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Ming View
- * 
+ *
  * @author		Luu Trong Hieu <hieuluutrong@vccorp.vn>
  * @version		$Id: View.php 4885 2010-10-08 08:33:20Z hieult $
  * @package		Ming
@@ -13,21 +13,21 @@ use Flywheel\Factory;
 use Flywheel\Html\Form;
 
 class Render {
-	/**
-	 * Assign templates vars
-	 * @var array
-	 */
-	private $_vars = array();
-	
-	private $_ext = '.phtml';
-	
-	/**
-	 * Template Path
-	 * @var string
-	 */
-	public $templatePath;
-	
-	public function __construct() {}
+    /**
+     * Assign templates vars
+     * @var array
+     */
+    private $_vars = array();
+
+    private $_ext = '.phtml';
+
+    /**
+     * Template Path
+     * @var string
+     */
+    public $templatePath;
+
+    public function __construct() {}
 
     /**
      * get HTMLDocument object
@@ -45,80 +45,80 @@ class Render {
      * @throws Exception
      * @return boolean    true if assign success
      */
-	public function assign($var, $value = null) {
-		if (is_string($var)) {
-			if ($var == '') {
-				return false;
-			}
-			$this->_vars[$var] = $value;
-			return true;
-		}
-		else if (is_array($var)) {
-			$this->_vars = array_merge($this->_vars, $var);
-			return true;
-		}
-		throw new Exception('assign() expects a string or array, received ' . gettype($var));
-	}
-	
-	/**
+    public function assign($var, $value = null) {
+        if (is_string($var)) {
+            if ($var == '') {
+                return false;
+            }
+            $this->_vars[$var] = $value;
+            return true;
+        }
+        else if (is_array($var)) {
+            $this->_vars = array_merge($this->_vars, $var);
+            return true;
+        }
+        throw new Exception('assign() expects a string or array, received ' . gettype($var));
+    }
+
+    /**
      * clear all the assigned templates variables.
      */
-	public function clearAllAssign() {
-		$this->_vars = array();
-	}
-	
-	/**
+    public function clearAllAssign() {
+        $this->_vars = array();
+    }
+
+    /**
      * clear the given assigned templates variable.
      *
      * @param string|array $vars the templates variables to clear
      *
      * @return boolean
      */
-	public function clearAssign($vars) {
-		if (is_array($vars)) {
-			for ($i = 0, $size = sizeof($vars); $i < $size; ++$i) {				
-				unset($this->_vars[$vars[$i]]);
-			}
-			return true;
-		}
-		unset($this->_vars[$vars]);
-	}
-	
-	/**
-	 * Render templates with data
-	 * 
-	 * @param string $file
-	 * @param array $vars. Default null
-	 * 
-	 * @return string
-	 * 
-	 * @throws Exception if file templates not found
-	 */
-	public function render($file, $vars = null) {
-		if (is_array($vars)) {
-			$this->assign($vars);			
-		}
-		
-		return $this->_render($file);
-	}
-	
-	/**
-	 * Display
-	 * 
-	 * @param string $file
-	 * @param array $vars
-	 * 
-	 * @return void
-	 * 
-	 * @throws Exception if file templates not found
-	 */
-	public function display($file, $vars = null) {
-		if (is_array($vars)) {
-			$this->assign($vars);
-		}		
-		
-		echo $this->_render($file);
-	}
+    public function clearAssign($vars) {
+        if (is_array($vars)) {
+            for ($i = 0, $size = sizeof($vars); $i < $size; ++$i) {
+                unset($this->_vars[$vars[$i]]);
+            }
+            return true;
+        }
+        unset($this->_vars[$vars]);
+    }
+
+    /**
+     * Render templates with data
+     *
+     * @param string $file
+     * @param array $vars. Default null
+     *
+     * @return string
+     *
+     * @throws Exception if file templates not found
+     */
+    public function render($file, $vars = null) {
+        if (is_array($vars)) {
+            $this->assign($vars);
+        }
+
+        return $this->_render($file);
+    }
+
+    /**
+     * Display
+     *
+     * @param string $file
+     * @param array $vars
+     *
+     * @return void
+     *
+     * @throws Exception if file templates not found
+     */
+    public function display($file, $vars = null) {
+        if (is_array($vars)) {
+            $this->assign($vars);
+        }
+
+        echo $this->_render($file);
+    }
 
     /**
      * Render
@@ -127,17 +127,18 @@ class Render {
      * @throws Exception
      * @return string
      */
-	private function _render($file) {
-		if (!file_exists($temFile = $this->templatePath .$file .$this->_ext)) {
-			throw new Exception('Template file not found:' .$temFile);
-		}
+    private function _render($file) {
+        $this->templatePath = preg_replace('#/+#','/', $this->templatePath);
+        if (!file_exists($temFile = $this->templatePath .$file .$this->_ext)) {
+            throw new Exception('Template file not found:' .$temFile);
+        }
 
-		extract($this->_vars, EXTR_SKIP);						
-		ob_start();
-		include $temFile;
-		$ouput = ob_get_clean();
-		return $ouput;		
-	}
+        extract($this->_vars, EXTR_SKIP);
+        ob_start();
+        include $temFile;
+        $ouput = ob_get_clean();
+        return $ouput;
+    }
 
     public function setFileExtension($ext) {
         $this->_ext = $ext;
@@ -176,11 +177,10 @@ class Render {
      * @param $route
      * @param array $params
      * @param string $ampersand
-     * @param bool $absolute
      * @return mixed
      */
-    public function createUrl($route, $params = array(), $ampersand='&', $absolute = true) {
-        return Factory::getRouter()->createUrl($route, $params, $ampersand, $absolute);
+    public function createUrl($route,$params=array(),$ampersand='&') {
+        return Factory::getRouter()->createUrl($route, $params, $ampersand);
     }
 
     public function createFrom($name = null, $action = '', $method = 'POST') {
