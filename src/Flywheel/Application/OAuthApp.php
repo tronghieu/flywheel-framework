4c2222a40ae698650b8617739b2e41b38db5c08c
Nguyen Xuan Kien <nguyenxuankien@alimama.vn>
2016-05-09 14:37:39 +0700

    fix load method

--- src/Flywheel/Application/OAuthApp.php
+++ src/Flywheel/Application/OAuthApp.php
@@ -9,6 +9,23 @@
 namespace Flywheel\Application;
 
 
-class OAuthApp extends ApiApp {
+use Flywheel\Exception;
+use Flywheel\Factory;
 
+class OAuthApp extends ApiApp {
+    protected function _loadMethod() {
+        /* @var \Flywheel\Router\WebRouter $router */
+        $router = Factory::getRouter();
+        $name = $router->getCamelControllerName();
+        $apiMethod = $router->getAction();
+        $class = $this->getAppNamespace() ."\\Controller\\{$name}";
+
+        if (!file_exists(($file = $this->_basePath.DIRECTORY_SEPARATOR
+            .'Controller'.DIRECTORY_SEPARATOR .str_replace("\\", DIRECTORY_SEPARATOR, $name) .'.php'))){
+            throw new Exception("Api's method {$class}/{$apiMethod} not found!", 404);
+        }
+
+        $this->_controller = new $class($name, $router->getParams());
+        return $this->_controller->execute($this->_requestMethod, $apiMethod);
+    }
 } 
